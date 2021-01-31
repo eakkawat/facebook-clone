f0347c594ac923e9b032126f4f278b82a4df3537
eakkawat <eakkawat.prommana.dev@gmail.com>
2021-01-31 22:02:45 +0700

    Add layout

---
+++ src/components/Facebook.vue
@@ -0,0 +1,144 @@
+<template>
+    <div class="bg-gray-100">
+        <header>
+            <nav class="flex justify-between items-center bg-white shadow-md">
+                <div class="flex items-center px-4 py-2">
+                    <svg class="text-white" viewBox="0 0 36 36" fill="url(#jsc_s_2)" height="40" width="40"><defs><linearGradient x1="50%" x2="50%" y1="97.0782153%" y2="0%" id="jsc_s_2"><stop offset="0%" stop-color="#0062E0"></stop><stop offset="100%" stop-color="#19AFFF"></stop></linearGradient></defs><path d="M15 35.8C6.5 34.3 0 26.9 0 18 0 8.1 8.1 0 18 0s18 8.1 18 18c0 8.9-6.5 16.3-15 17.8l-1-.8h-4l-1 .8z"></path><path class="p361ku9c" d="M25 23l.8-5H21v-3.5c0-1.4.5-2.5 2.7-2.5H26V7.4c-1.3-.2-2.7-.4-4-.4-4.1 0-7 2.5-7 7v4h-4.5v5H15v12.7c1 .2 2 .3 3 .3s2-.1 3-.3V23h4z"></path></svg>
+                    <input type="text" class="ml-2 py-2 px-4 bg-gray-100 rounded-full text-gray-600 focus:outline-none w-60" placeholder="Search on Facebook">
+                </div>
+                <div class="flex flex-grow justify-around mx-2">
+                    <a href="#" class="border-b-2 border-blue-500 pb-3 pt-3 mt-1 px-10">
+                        <svg class="fill-current text-blue-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M8 20H3V10H0L10 0l10 10h-3v10h-5v-6H8v6z"/></svg>
+                    </a>
+                    <a href="#" class="pt-3 px-10 hover:bg-gray-100 rounded-lg my-1 relative">
+                        <svg class="fill-current text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM7 6l8 4-8 4V6z"/></svg>
+                        <span class="bg-red-500 absolute rounded-full text-white px-1 text-sm top-0 right-6">9+</span>
+                    </a>
+                    <a href="#" class="pt-3 px-10 hover:bg-gray-100 rounded-lg my-1">
+                        <svg class="fill-current text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M18 9.87V20H2V9.87a4.25 4.25 0 0 0 3-.38V14h10V9.5a4.26 4.26 0 0 0 3 .37zM3 0h4l-.67 6.03A3.43 3.43 0 0 1 3 9C1.34 9 .42 7.73.95 6.15L3 0zm5 0h4l.7 6.3c.17 1.5-.91 2.7-2.42 2.7h-.56A2.38 2.38 0 0 1 7.3 6.3L8 0zm5 0h4l2.05 6.15C19.58 7.73 18.65 9 17 9a3.42 3.42 0 0 1-3.33-2.97L13 0z"/></svg>                        
+                    </a>
+                    <a href="#" class="pt-3 px-10 hover:bg-gray-100 rounded-lg my-1">
+                        <svg class="fill-current text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0 1c2.15 0 4.2.4 6.1 1.09L12 16h-1.25L10 20H4l-.75-4H2L.9 10.09A17.93 17.93 0 0 1 7 9zm8.31.17c1.32.18 2.59.48 3.8.92L18 16h-1.25L16 20h-3.96l.37-2h1.25l1.65-8.83zM13 0a4 4 0 1 1-1.33 7.76 5.96 5.96 0 0 0 0-7.52C12.1.1 12.53 0 13 0z"/></svg>
+                    </a>
+                    <a href="#" class="pt-3 px-10 hover:bg-gray-100 rounded-lg my-1 relative">
+                        <svg class="fill-current text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 0h9v9H0V0zm2 2v5h5V2H2zm-2 9h9v9H0v-9zm2 2v5h5v-5H2zm9-13h9v9h-9V0zm2 2v5h5V2h-5zm-2 9h9v9h-9v-9zm2 2v5h5v-5h-5z"/></svg>
+                        <span class="bg-red-500 absolute rounded-full text-white px-1 text-sm top-0 right-6">9+</span>
+
+                    </a>
+                </div>
+                <div>
+                    <a href="#" class="flex items-center">
+                        <img class="rounded-full w-7 h-7" src="https://scontent.fcnx2-1.fna.fbcdn.net/v/t1.0-1/cp0/p80x80/82315401_10221385039072638_1539097244144238592_n.jpg?_nc_cat=103&ccb=2&_nc_sid=7206a8&_nc_ohc=sLOxxSDoIZEAX8oT8JD&_nc_ht=scontent.fcnx2-1.fna&tp=27&oh=d09b4b2a7a2d72683136da4f610f0767&oe=603D02F2" alt="">
+                        <span class="ml-2 text-gray-900 font-semibold text-sm">Eakkawat</span>
+                    </a>
+                </div>
+                <div class="flex mx-4">
+                    <a href="#" >
+                        <svg class="fill-current text-gray-900 rounded-full bg-gray-200 w-10 h-10 p-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                        </svg>
+                    </a>
+                    <a href="#">
+                        <svg class="ml-2 fill-current text-gray-900 rounded-full bg-gray-200 w-10 h-10 p-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                    <a href="#">
+                        <svg class="ml-2 fill-current text-gray-900 rounded-full bg-gray-200 w-10 h-10 p-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+                        </svg>
+                    </a>
+                    <a href="#">
+                        <svg class="ml-2 fill-current text-gray-900 rounded-full bg-gray-200 w-10 h-10 p-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                </div>
+            </nav>
+        </header>
+        <main class="flex justify-between min-h-screen">
+            <aside class="my-4 mx-4">
+                <ul>
+                    <li class="py-3 pr-14 px-4 rounded-lg hover:bg-gray-200"><a class="font-semibold" href="#">Eakkawat Prommana</a></li>
+                    <li class="py-3 pr-14 px-4 rounded-lg hover:bg-gray-200"><a class="font-semibold" href="#">Covid-19: info</a></li>
+                    <li class="py-3 pr-14 px-4 rounded-lg hover:bg-gray-200"><a class="font-semibold" href="#">Pages</a></li>
+                    <li class="py-3 pr-14 px-4 rounded-lg hover:bg-gray-200"><a class="font-semibold" href="#">Friends </a></li>
+                    <li class="py-3 pr-14 px-4 rounded-lg hover:bg-gray-200"><a class="font-semibold" href="#">Marketplace</a></li>
+                </ul>
+            </aside>
+            <section class="my-6">
+                <div class="flex justify-around">
+                    <a href="#" class="ml-2">
+                        <img class="rounded-lg w-28 h-42" src="https://scontent.fcnx2-1.fna.fbcdn.net/v/t15.5256-10/p235x165/131047783_3962304360466887_5721464940042291449_n.jpg?_nc_cat=108&ccb=2&_nc_sid=1055be&_nc_ohc=wESjQSXi0PgAX_dLYsU&_nc_ht=scontent.fcnx2-1.fna&oh=2802a2f8985ed7acb108ef8049695e39&oe=603C1029" alt="">
+                    </a>
+                    <a href="#" class="ml-2">
+                        <img class="rounded-lg w-28 h-42" src="https://scontent.fcnx2-1.fna.fbcdn.net/v/t15.5256-10/p235x165/131047783_3962304360466887_5721464940042291449_n.jpg?_nc_cat=108&ccb=2&_nc_sid=1055be&_nc_ohc=wESjQSXi0PgAX_dLYsU&_nc_ht=scontent.fcnx2-1.fna&oh=2802a2f8985ed7acb108ef8049695e39&oe=603C1029" alt="">
+                    </a>
+                    <a href="#" class="ml-2">
+                        <img class="rounded-lg w-28 h-42" src="https://scontent.fcnx2-1.fna.fbcdn.net/v/t15.5256-10/p235x165/131047783_3962304360466887_5721464940042291449_n.jpg?_nc_cat=108&ccb=2&_nc_sid=1055be&_nc_ohc=wESjQSXi0PgAX_dLYsU&_nc_ht=scontent.fcnx2-1.fna&oh=2802a2f8985ed7acb108ef8049695e39&oe=603C1029" alt="">
+                    </a>
+                    <a href="#" class="ml-2">
+                        <img class="rounded-lg w-28 h-42" src="https://scontent.fcnx2-1.fna.fbcdn.net/v/t15.5256-10/p235x165/131047783_3962304360466887_5721464940042291449_n.jpg?_nc_cat=108&ccb=2&_nc_sid=1055be&_nc_ohc=wESjQSXi0PgAX_dLYsU&_nc_ht=scontent.fcnx2-1.fna&oh=2802a2f8985ed7acb108ef8049695e39&oe=603C1029" alt="">
+                    </a>
+                    <a href="#" class="ml-2">
+                        <img class="rounded-lg w-28 h-42" src="https://scontent.fcnx2-1.fna.fbcdn.net/v/t15.5256-10/p235x165/131047783_3962304360466887_5721464940042291449_n.jpg?_nc_cat=108&ccb=2&_nc_sid=1055be&_nc_ohc=wESjQSXi0PgAX_dLYsU&_nc_ht=scontent.fcnx2-1.fna&oh=2802a2f8985ed7acb108ef8049695e39&oe=603C1029" alt="">
+                    </a>
+                </div>
+            </section>
+            <aside class="m-4">
+                <div class="border-b border-gray-300 pb-2">
+                    <h1 class="text-gray-500 font-bold pl-4">Birthday</h1>
+                    <a href="#" class="font-semibold text-gray-900 block mt-2 py-2 px-4 rounded-lg hover:bg-gray-200">
+                        <span class=" text-gray-600 font-normal">Today is birthday of</span>
+                        Looked Looked
+                    </a>
+                </div>
+                <div class="mt-4">
+                    <h1 class="text-gray-500 font-bold pl-4">Contacts</h1>
+                    <ul class="mt-2">
+                        <li class="py-3 px-4 hover:bg-gray-200 rounded-lg flex">
+                            <div class="relative">
+                                <img class="rounded-full w-7 h-7" src="https://scontent.fcnx2-1.fna.fbcdn.net/v/t1.0-1/cp0/s56x56/118011828_3508326532531341_2634792442163657353_n.jpg?_nc_cat=100&ccb=2&_nc_sid=7206a8&_nc_ohc=pgOBugCU28EAX94vjWA&_nc_ht=scontent.fcnx2-1.fna&tp=28&oh=b9aa442ea08008edc8cf9f94aac0c776&oe=603A4FC8" alt="">
+                                <span class="w-2.5 h-2.5 rounded-full bg-green-400 absolute bottom-0 right-0 border-2"></span>
+                            </div>
+                            <a class="text-gray-900 font-semibold ml-2" href="#">Parichart Prommana</a>
+                        </li>
+                        <li class="py-3 px-4 hover:bg-gray-200 rounded-lg flex">
+                            <div class="relative">
+                                <img class="rounded-full w-7 h-7" src="https://scontent.fcnx2-1.fna.fbcdn.net/v/t1.0-1/cp0/s56x56/118011828_3508326532531341_2634792442163657353_n.jpg?_nc_cat=100&ccb=2&_nc_sid=7206a8&_nc_ohc=pgOBugCU28EAX94vjWA&_nc_ht=scontent.fcnx2-1.fna&tp=28&oh=b9aa442ea08008edc8cf9f94aac0c776&oe=603A4FC8" alt="">
+                                <span class="w-2.5 h-2.5 rounded-full bg-green-400 absolute bottom-0 right-0 border-2"></span>
+                            </div>
+                            <a class="text-gray-900 font-semibold ml-2" href="#">Parichart Prommana</a>
+                        </li>
+                        <li class="py-3 px-4 hover:bg-gray-200 rounded-lg flex">
+                            <div class="relative">
+                                <img class="rounded-full w-7 h-7" src="https://scontent.fcnx2-1.fna.fbcdn.net/v/t1.0-1/cp0/s56x56/118011828_3508326532531341_2634792442163657353_n.jpg?_nc_cat=100&ccb=2&_nc_sid=7206a8&_nc_ohc=pgOBugCU28EAX94vjWA&_nc_ht=scontent.fcnx2-1.fna&tp=28&oh=b9aa442ea08008edc8cf9f94aac0c776&oe=603A4FC8" alt="">
+                                <span class="w-2.5 h-2.5 rounded-full bg-green-400 absolute bottom-0 right-0 border-2"></span>
+                            </div>
+                            <a class="text-gray-900 font-semibold ml-2" href="#">Parichart Prommana</a>
+                        </li>
+                        <li class="py-3 px-4 hover:bg-gray-200 rounded-lg flex">
+                            <div class="relative">
+                                <img class="rounded-full w-7 h-7" src="https://scontent.fcnx2-1.fna.fbcdn.net/v/t1.0-1/cp0/s56x56/118011828_3508326532531341_2634792442163657353_n.jpg?_nc_cat=100&ccb=2&_nc_sid=7206a8&_nc_ohc=pgOBugCU28EAX94vjWA&_nc_ht=scontent.fcnx2-1.fna&tp=28&oh=b9aa442ea08008edc8cf9f94aac0c776&oe=603A4FC8" alt="">
+                                <span class="w-2.5 h-2.5 rounded-full bg-green-400 absolute bottom-0 right-0 border-2"></span>
+                            </div>
+                            <a class="text-gray-900 font-semibold ml-2" href="#">Parichart Prommana</a>
+                        </li>
+                        <li class="py-3 px-4 hover:bg-gray-200 rounded-lg flex">
+                            <div class="relative">
+                                <img class="rounded-full w-7 h-7" src="https://scontent.fcnx2-1.fna.fbcdn.net/v/t1.0-1/cp0/s56x56/118011828_3508326532531341_2634792442163657353_n.jpg?_nc_cat=100&ccb=2&_nc_sid=7206a8&_nc_ohc=pgOBugCU28EAX94vjWA&_nc_ht=scontent.fcnx2-1.fna&tp=28&oh=b9aa442ea08008edc8cf9f94aac0c776&oe=603A4FC8" alt="">
+                                <span class="w-2.5 h-2.5 rounded-full bg-green-400 absolute bottom-0 right-0 border-2"></span>
+                            </div>
+                            <a class="text-gray-900 font-semibold ml-2" href="#">Parichart Prommana</a>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
+        </main>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
